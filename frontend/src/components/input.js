2b5091ea9978db3075b004fa104f12829f2be39b
Necati Berimen <berimen66@gmail.com>
2021-07-10 22:31:25 +0300

    Create input

---
+++ frontend/src/components/input.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Input = (props) => {
+
+    const {label, error, name, onChange, type}= props;
+    const className=error ? "form-control is-invalid" : "form-control";
+    return (
+        <div className="mb-3">
+            <label>{label}</label>
+            <input
+                className={className}
+                name={name}
+                onChange={onChange}
+                type={type}
+            />
+            <div className="invalid-feedback">
+                {props.error}
+            </div>
+        </div>
+    )
+}
+
+export default Input;
