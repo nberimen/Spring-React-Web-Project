cb34222fb26c27121a8f6cea91afddf423ab9b9e
Necati Berimen <berimen66@gmail.com>
2021-07-14 23:13:20 +0300

    Create CurrentUser.java

---
+++ hoaxify/src/main/java/com/nberimen/shared/CurrentUser.java
@@ -0,0 +1,16 @@
+package com.nberimen.shared;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@Target({ElementType.PARAMETER})
+@Retention(RUNTIME)
+@AuthenticationPrincipal
+public @interface CurrentUser {
+
+}
