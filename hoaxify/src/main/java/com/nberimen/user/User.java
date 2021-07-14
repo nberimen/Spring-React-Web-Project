310627c5300f9c344760223ddd3450005a12f0ed
Necati Berimen <berimen66@gmail.com>
2021-07-14 23:13:31 +0300

    Update User.java

--- hoaxify/src/main/java/com/nberimen/user/User.java
+++ hoaxify/src/main/java/com/nberimen/user/User.java
@@ -1,5 +1,7 @@
 package com.nberimen.user;
 
+import java.util.Collection;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -7,27 +9,71 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.fasterxml.jackson.annotation.JsonView;
+import com.nberimen.shared.Views;
+
 import lombok.Data;
 
 @Entity
 @Data
-public class User {
+public class User implements UserDetails {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue
+	@JsonView(Views.Base.class)
 	private long id;
 	
 	@NotNull(message = "{hoaxify.constraint.username.NotNull.message}")
 	@Size(min = 4, max=255)
 	@UniqueUsername
+	@JsonView(Views.Base.class)
 	private String username;
 	
 	@NotNull
 	@Size(min = 4, max=255)
+	@JsonView(Views.Base.class)
 	private String displayName;
 	
 	@NotNull
 	@Size(min = 8, max=255)
 	@Pattern(regexp ="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{hoaxify.constraint.password.Pattern.message}")
 	private String password;
+	
+	@JsonView(Views.Base.class)
+	private String image;
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return AuthorityUtils.createAuthorityList("Role_user");
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
