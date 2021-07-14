562e21e3b9e3f5fad41d8a5100cca7a95439feb6
Necati Berimen <berimen66@gmail.com>
2021-07-14 23:11:38 +0300

    Create ButtonWithProgress.js

---
+++ frontend/src/components/ButtonWithProgress.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const ButtonWithProgress = (props) => {
+    const {onClick, pendingApiCall, disabled, text} = props;
+    return (
+        <button
+            className="btn btn-primary"
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {pendingApiCall && <span className="spinner-border spinner-border-sm text-light"></span>}
+            {text}
+        </button>
+    )
+}
+
+export default ButtonWithProgress
