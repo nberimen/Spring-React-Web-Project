faad553c06c92b71b351805d20ac92ce2f8b4a1a
Necati Berimen <berimen66@gmail.com>
2021-07-14 23:13:17 +0300

    Update HoaxifyApplication.java

--- hoaxify/src/main/java/com/nberimen/HoaxifyApplication.java
+++ hoaxify/src/main/java/com/nberimen/HoaxifyApplication.java
@@ -1,14 +1,28 @@
 package com.nberimen;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+import com.nberimen.user.User;
+import com.nberimen.user.UserService;
+
+@SpringBootApplication
 public class HoaxifyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HoaxifyApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner createInitialUsers(UserService userService) {
+		return (args) -> {
+				User user = new User();
+				user.setUsername("user1");
+				user.setDisplayName("display1");
+				user.setPassword("Password1");
+				userService.save(user);
+		};
+	}
 }
