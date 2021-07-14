225bddbd291afc066d072aeba2763e79c043e499
Necati Berimen <berimen66@gmail.com>
2021-07-14 23:13:37 +0300

    Update UserController.java

--- hoaxify/src/main/java/com/nberimen/user/UserController.java
+++ hoaxify/src/main/java/com/nberimen/user/UserController.java
@@ -1,21 +1,12 @@
 package com.nberimen.user;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus; 
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nberimen.error.ApiError;
 import com.nberimen.shared.GenericResponse;
 
 @RestController
@@ -33,16 +24,5 @@ public class UserController {
 		
 	}
 	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ApiError handleValidationException(MethodArgumentNotValidException exception) {
-		ApiError error = new ApiError(400,"Validation error", "/api/1.0/users");
-		Map<String, String> validationErrors = new HashMap<>();
-		for(FieldError fieldError:exception.getBindingResult().getFieldErrors()) {
-			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-		}
-		error.setValidationErrors(validationErrors);
-		return error;
-	}
 }
 
