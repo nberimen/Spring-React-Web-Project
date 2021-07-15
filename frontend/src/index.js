823d0750ff6285a5019856fda33aa927003f75db
Necati Berimen <berimen66@gmail.com>
2021-07-15 20:25:31 +0300

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import './bootstrap-override.scss'
-import App from './App';
-import UserSignupPage from './pages/UserSingupPage';
 import reportWebVitals from './reportWebVitals';
 import './i18n'
-
-ReactDOM.render(<UserSignupPage />, document.getElementById('root')
+import App from './container/App'
+ReactDOM.render(
+   <App/>
+    , document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
