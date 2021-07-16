0a24d3bd955a102a72470a55936ed72bd0bb81c0
Necati Berimen <berimen66@gmail.com>
2021-07-16 17:23:06 +0300

    Update ApiProgress.js

--- frontend/src/shared/ApiProgress.js
+++ frontend/src/shared/ApiProgress.js
@@ -13,18 +13,28 @@ export function withApiProgress(WrappedComponent, apiPath) {
         }
 
         componentDidMount() {
-            axios.interceptors.request.use((request) => {
-                this.updateApiCallFor(request.url, true);
-                return request;
-            })
-            axios.interceptors.response.use(response => {
-                this.updateApiCallFor(response.config.url, false);
-                return response;
-            }, error => {
-                this.updateApiCallFor(error.config.url, false);
-                throw error;
-            }
+            this.requestInterceptor = axios.interceptors.request.use(
+                request => {
+                    console.log("nb", apiPath)
+                    this.updateApiCallFor(request.url, true);
+                    return request;
+                })
+            this.responseInterceptor = axios.interceptors.response.use(
+                response => {
+                    this.updateApiCallFor(response.config.url, false);
+                    return response;
+                },
+                error => {
+                    this.updateApiCallFor(error.config.url, false);
+                    throw error;
+                }
             )
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.response.eject(this.responseInterceptor);
+
         }
         updateApiCallFor = (url, inProgress) => {
             if (url === apiPath) {
