4ceb262febe3c10e2d0aafa8b65913b5444b399d
Necati Berimen <berimen66@gmail.com>
2021-09-10 16:08:37 +0300

    Delete Hoax Api

--- hoaxify/src/main/java/com/nberimen/hoax/HoaxController.java
+++ hoaxify/src/main/java/com/nberimen/hoax/HoaxController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -68,6 +69,12 @@ public class HoaxController {
 	Page<HoaxVM> getUserHoaxes(@PathVariable String username,
 			@PageableDefault(sort = "id", direction = Direction.DESC) Pageable page) {
 		return hoaxService.getHoaxesOfUser(username, page).map(HoaxVM::new);
+	}
+	
+	@DeleteMapping("/hoaxes/{id:[0-9]+}")
+	GenericResponse deleteHoax(@PathVariable long id) {
+		hoaxService.delete(id);
+		return new GenericResponse("Hoax removed");
 	}
 
 }
--- hoaxify/src/main/java/com/nberimen/hoax/HoaxService.java
+++ hoaxify/src/main/java/com/nberimen/hoax/HoaxService.java
@@ -96,4 +96,8 @@ public class HoaxService {
 			return criteriaBuilder.greaterThan(root.get("id"), id);
 		};
 	}
+
+	public void delete(long id) {
+		hoaxRepository.deleteById(id);
+	}
 }
